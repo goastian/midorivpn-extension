8aa9a73efb81ac4a4b48f0e8e2cf2c9e98bb6ece
Im-Jota <joseluisromeroarce12@gmail.com>
2025-06-02 11:28:52 -0500

    feat: add pinia and storage plugin

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
+import { chromeStoragePlugin } from './plugins/chromeStoragePlugin';
 import App from './App.vue';
 import './style.css';
 
 const app = createApp(App);
+const pinia = createPinia();
+
+pinia.use(chromeStoragePlugin);
+app.use(pinia);
 
 app.mount('#App');

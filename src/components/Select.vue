6067ae0e32b233eea3a945a8aa55386d0cc4fe85
Im-Jota <joseluisromeroarce12@gmail.com>
2025-06-02 11:26:38 -0500

    feat: add servers dropdown component

---
+++ src/components/Select.vue
@@ -0,0 +1,17 @@
+<template>
+    <select>
+        <option>United State</option>
+    </select>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+select {
+    padding: .4rem 1rem;
+    border: none;
+    border: 1px solid rgba(0, 0, 0, 0.1);
+    border-radius: .4rem;
+}
+</style>

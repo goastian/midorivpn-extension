4ba2c09ecdf537dbd90940abb16854ed22c74076
Im-Jota <joseluisromeroarce12@gmail.com>
2025-06-16 08:58:25 -0500

    feat: save only specific data to storage

--- src/plugins/chromeStoragePlugin.js
+++ src/plugins/chromeStoragePlugin.js
@@ -3,6 +3,8 @@
 export function chromeStoragePlugin({ store }) {
 
     const storageKey = `${store.$id}`;
+    // Leer persistFields desde el getter
+    const persistFields = store.persistFields || null;
 
     chrome.storage.local.get([storageKey], (result) => {
         if (result[storageKey]) {
@@ -11,7 +13,15 @@ export function chromeStoragePlugin({ store }) {
     });
 
     store.$subscribe((_mutation, state) => {
-        const rawState = JSON.parse(JSON.stringify(state));
+        let rawState = JSON.parse(JSON.stringify(state));
+
+        if (persistFields) {
+            rawState = persistFields.reduce((acc, key) => {
+                if (key in rawState) acc[key] = rawState[key];
+                return acc;
+            }, {});
+        }
+
         chrome.storage.local.set({ [storageKey]: rawState });
     });
 }

c0c479d26bad005e016e3eb3d96208fb8c5dea45
Im-Jota <joseluisromeroarce12@gmail.com>
2025-06-17 11:24:41 -0500

    feat: add Pinia store for VPN config with local persistence

---
+++ src/stores/useVpnStore.js
@@ -0,0 +1,48 @@
+import { defineStore } from "pinia";
+import axios from 'axios';
+import Token from '../utils/token';
+
+const useServerStore = defineStore('server', {
+    state: () => ({
+        id: null,
+    }),
+
+    actions: {
+        async createDevice() {
+            const validated = new Token();
+            const token = await validated.getDecryptedToken();
+            try {
+                const params = {
+                    name: 'extension'
+                }
+
+                if (this.id) {
+                    params.id = this.id;
+                }
+
+                const res = await axios.post(`${process.env.PASSPORT_DOMAIN_SERVER}/api/v1/users/vpn/devices`, params, {
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        Accept: 'application/json',
+                        Authorization: `Bearer ${token}`
+                    },
+                });
+
+                if (res.status == 201) {
+                    this.id = res.data.data.id;
+                }
+            } catch (error) {
+                // En caso de error, guardamos el error en el estado
+                console.log(error.response)
+            }
+        },
+    },
+
+    getters: {
+        persistFields() {
+            return ['id'];
+        },
+    },
+});
+
+export default useServerStore;

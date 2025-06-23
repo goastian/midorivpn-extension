46a98399b0eda0943058be75f6587ef8524d4ff7
Im-Jota <joseluisromeroarce12@gmail.com>
2025-06-22 21:52:27 -0500

    feat: add server store

--- src/stores/useVpnStore.js
+++ src/stores/useVpnStore.js
@@ -1,13 +1,40 @@
 import { defineStore } from "pinia";
 import axios from 'axios';
 import Token from '../utils/token';
+import serverManager from '../service/servers.js';
 
 const useServerStore = defineStore('server', {
     state: () => ({
+        servers: [],
+        active: null,
         id: null,
     }),
 
     actions: {
+        async loadServers() {
+            const validated = new Token();
+            const token = await validated.getDecryptedToken();
+            const isValidated = await validated.verificate();
+            if (isValidated) {
+                const ser = serverManager.getServers();
+                console.log(ser);
+                const res = await axios.get(`${process.env.PASSPORT_DOMAIN_SERVER}/api/v1/users/vpn/servers`, {
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        Accept: 'application/json',
+                        Authorization: `Bearer ${token}`
+                    },
+                });
+                if (res.status === 200) {
+                    console.log(res.data.data)
+                    this.servers = { ...res.data.data };
+                    if (!this.active) {
+                        this.active = { ...res.data.data[0] }
+                    }
+                }
+            }
+        },
+
         async createDevice() {
             const validated = new Token();
             const token = await validated.getDecryptedToken();
@@ -39,7 +66,7 @@ const useServerStore = defineStore('server', {
 
     getters: {
         persistFields() {
-            return ['id'];
+            return ['id', 'active', 'servers'];
         },
     },
 });
